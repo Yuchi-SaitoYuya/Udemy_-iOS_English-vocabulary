3d8eed7325b057e874186b31c69289b7d33dc3b9
Yuchinante <ysuu45143745@gmail.com>
2025-04-12 16:44:34 +0900

    セクション12

--- Udemy_-iOS_English vocabulary/ViewController.swift
+++ Udemy_-iOS_English vocabulary/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var wordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        wordLabel.text = "Apple"
     }
 
-
+    @IBAction func showAnswer(_ sender: Any) {
+        wordLabel.text = "りんご"
+    }
 }
 
