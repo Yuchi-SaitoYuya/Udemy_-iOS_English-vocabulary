7828585ea3850c7a9385e7f898cc681fc054299c
Yuchinante <ysuu45143745@gmail.com>
2025-04-21 23:39:08 +0900

    配列を追加

--- Udemy_-iOS_English vocabulary/ViewController.swift
+++ Udemy_-iOS_English vocabulary/ViewController.swift
@@ -11,15 +11,23 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var wordLabel: UILabel!
-    
+
+    let words: [[String]] = [
+        ["write", "書く"],
+        ["go", "行く"],
+        ["price", "価格"],
+        ["cheap", "安い"],
+        ["shop", "店"]
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        wordLabel.text = "Apple"
+        wordLabel.text = words[0][0]
     }
 
     @IBAction func showAnswer(_ sender: Any) {
-        wordLabel.text = "りんご"
+        wordLabel.text = words[0][1]
     }
 }
 
